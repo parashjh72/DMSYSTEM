2940c04cba1ab9dc0e3a79eec203a91c228d4c71
Parash <parash@Parashs-Mac-mini.local>
2026-09-22 22:17:04 +0545

    chore(diag): respond with error details when dbg=1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,14 +15,16 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (\Throwable $e, Request $request) {
-            if ($request->query('show_err') === 'k7Qx9mP2wL5vR8nT3jF6hB4dS1yG0zA') {
+        $exceptions->respond(function ($response, \Throwable $e, Request $request) {
+            if ($request->query('dbg') === '1') {
                 return response()->json([
                     'exception' => get_class($e),
                     'message' => $e->getMessage(),
                     'file' => $e->getFile() . ':' . $e->getLine(),
                 ], 500);
             }
+
+            return $response;
         });
 
         $exceptions->shouldRenderJsonWhen(
